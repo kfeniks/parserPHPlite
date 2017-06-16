6863a52aac38dfeac3004ca21666fdbdb76a4f52
kfeniks <prinz.haruhi@gmail.com>
2017-06-17 00:01:20 +0300

    init

--- app/test/03.php
+++ app/test/03.php
@@ -4,4 +4,8 @@
 
             header("Content-Type: text/html; charset=utf-8");
 
-           
+            $html = file_get_html('http://keccgroup.ru/price');
+
+                //Save as file .txt
+                file_put_contents( 'price_parser.html', $html);
+                echo "All is OK";
