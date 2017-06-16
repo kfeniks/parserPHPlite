2e53a252ca7eaa48eabf4daf73f945f47c4a7bbb
kfeniks <prinz.haruhi@gmail.com>
2017-06-16 23:40:06 +0300

    init

--- app/test/03.php
+++ app/test/03.php
@@ -4,8 +4,4 @@
 
             header("Content-Type: text/html; charset=utf-8");
 
-            $html = file_get_html('http://keccgroup.ru/price');
-
-        // Find all price
-        foreach($html->find('tr') as $element)
-            echo $element->find('td', 2) . '<br>';
+           
